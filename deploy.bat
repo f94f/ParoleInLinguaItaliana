6fe23a1ccbd8a6927731056e6a2c3a66fc3100ba
Felipe <f.mate10@uniandes.edu.co>
2024-01-31 22:25:30 +0100

    ajustando

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New deployment'
-git push -f https://github.com/f94f/ParoleInLinguaItaliana.git main:gh-pages
+git push -f git@github.com:f94f/ParoleInLinguaItaliana.git main:gh-pages
 
 cd -
