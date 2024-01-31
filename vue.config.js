c3861080c6e442fe26b5c50f6707e0f9f1abb6d5
Felipe <f.mate10@uniandes.edu.co>
2024-01-31 21:48:04 +0100

    agregado config para pages

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: '/vue-pages/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
